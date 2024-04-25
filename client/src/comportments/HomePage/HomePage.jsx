51c499a0e0755e624f4afdc55cb01256cdeca3cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 11:43:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -4,7 +4,12 @@ const HomePage = () => {
   return (
     <div>
         <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp10890262.jpg)] bg-center bg-cover lg:h-[80vh] h-screen w-full'>
-            Hi all
+            <div className="text-white text-center my-12">
+                <h1 className="text-3xl font-semibold">Welcome to Library of NIFS</h1>
+            </div>
+        </div>
+        <div className="bg-white lg:mx-24 mx-8 py-16 px-12 lg:mt-[-100px] mt-[-280px] rounded shadow-2xl lg:mb-40 mb-20">
+            Library
         </div>
     </div>
   )
