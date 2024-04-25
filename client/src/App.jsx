25fc106eaa7deb26995ec3cefb454e4100930f03
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 13:36:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./comportments/HomePage/HomePage";
 import SignIn from "./comportments/SignInSignUp/SignIn";
+import SignUp from "./comportments/SignInSignUp/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}/>
+        <Route path="/" element={<HomePage />} />
         <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
