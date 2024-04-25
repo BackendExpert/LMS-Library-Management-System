9a1c70f3cdfd47481b00bd318a576551f3c70d53
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 15:48:36 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -2,16 +2,35 @@ import React from 'react'
 import Navbar from './Navbar'
 import MyIcons from '@reacticons/ionicons'
 import Footer from './Footer'
+import  secureLocalStorage  from  "react-secure-storage";
+import { Link, useNavigate } from 'react-router-dom';
 
 // https://wallpapercave.com/wp/wp10395041.jpg
 // https://wallpapercave.com/wp/wp10395058.jpg
 const HomePage = () => {
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
   return (
     <div>
         <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp10395041.jpg)] bg-center bg-cover lg:h-[80vh] h-screen w-full'>
             <Navbar />
             <div className="text-white text-center my-12">
                 <h1 className="text-3xl font-semibold">Welcome to Library of NIFS</h1>
+                {
+                    (() => {
+                        if(RoleUser !== null && EmailUser !== null){
+                            return (
+                                <div className="my-4">
+                                    <Link to={'/Dashboard'}>
+                                        <button className='bg-white rounded py-2 px-8 text-gray-500 duration-500 hover:ml-4'>to Dashbord</button>
+                                    </Link>
+                                </div>
+                            )
+                        }
+                    })()
+                }
             </div>
         </div>
         <div className="bg-white lg:mx-24 mx-8 py-16 px-12 lg:my-[-100px] my-[-280px] rounded shadow-2xl lg:mb-40 mb-20">
