2087740c610bf12eedf90aa285d7d164ca604a16
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 11:49:06 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const HomePage = () => {
   return (
     <div>
         <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp10890262.jpg)] bg-center bg-cover lg:h-[80vh] h-screen w-full'>
+            <Navbar />
             <div className="text-white text-center my-12">
                 <h1 className="text-3xl font-semibold">Welcome to Library of NIFS</h1>
             </div>
