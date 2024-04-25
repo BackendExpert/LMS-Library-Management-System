f983707be844025c486192ae74e8f71d549c5a78
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 16:22:27 +0530

    Update DashFooter.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashFooter.jsx
+++ client/src/comportments/Dashboard/DashFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DashFooter = () => {
   return (
     <div className="bg-white rounded-2xl pr-5 shadow-xl mt-8 my-2 py-12 text-center">
-        &copy; VRS - Vehicle rental System <br />
+        &copy; LMS - Library Management System <br />
         Developed and Design by National Institute of Fundamental Studies
     </div>
   )
