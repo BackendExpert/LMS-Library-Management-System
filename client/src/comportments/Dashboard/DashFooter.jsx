d7412fb76f864bcb802a9b283cfcc493ca5cd8cc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 16:20:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashFooter.jsx
+++ client/src/comportments/Dashboard/DashFooter.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const DashFooter = () => {
   return (
-    <div>DashFooter</div>
+    <div className="bg-white rounded pr-5 shadow-xl mt-8 py-12 text-center">
+        &copy; VRS - Vehicle rental System <br />
+        Developed and Design by JehanKandy
+    </div>
   )
 }
 
--- client/src/comportments/Dashboard/SummaryDash.jsx
+++ client/src/comportments/Dashboard/SummaryDash.jsx
@@ -202,10 +202,7 @@ const SummaryDash = () => {
 
 
                 </div>
-                <div className="bg-white rounded pr-5 shadow-xl mt-8 py-12 text-center">
-                    &copy; VRS - Vehicle rental System <br />
-                    Developed and Design by JehanKandy
-                </div>
+
             </div>
         )
     }
