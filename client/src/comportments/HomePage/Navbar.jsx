ba0663bdb16444fe47982806a8307899a2b3aab3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 11:47:33 +0530

    Update Navbar.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

---
+++ client/src/comportments/HomePage/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import MyIcons from '@reacticons/ionicons'
+import axios from 'axios'
+import  secureLocalStorage  from  "react-secure-storage";
+
+
+const Navbar = () => {
+  return (
+    <div className='mb-20'>
+        <div className="bg-none text-white mt-[-20px]">
+            <div className="flex justify-between mx-16">
+                <div className="flex">
+                    <MyIcons name='train' size='large' ></MyIcons>
+                    <h1 className="my-1">Train Booking</h1>
+                </div>
+                <div className="my-1">                   
+                    <div className="">                        
+                        <Link to={'/SignIn'}>
+                            <p className="font-semibold duration-500 hover:mr-2" >Sign IN</p>
+                        </Link>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
