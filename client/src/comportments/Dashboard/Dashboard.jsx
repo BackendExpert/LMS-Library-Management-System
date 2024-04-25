6baa95424901cc519c2f1699b36ebe6d461c9aec
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 15:57:06 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashboard.jsx
+++ client/src/comportments/Dashboard/Dashboard.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    const navigate = useNavigate() 
+
+    //for open and close Side bar
+    const [sideOpen, SetsideOpen] = useState();
+    const [navOpen, SetNavOpen] = useState();
+
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+    
+    const allUserSide = [
+        {id: 1, name: "Vehicles", link: "/Vehicles", icon: <Icons name="car" size="large"></Icons>},
+        {id: 2, name: "Rented Vehicle", link: "#", icon: <Icons name="car-sport" size="large"></Icons>},
+        {id: 3, name: "Own Vehicle ", link: "#", icon: <Icons name="car" size="large"></Icons>},
+        {id: 4, name: "Profile", link: "#", icon: <Icons name="person" size="large"></Icons>},
+        {id: 5, name: "Users", link: "#", icon: <Icons name="people" size="large"></Icons>},
+        {id: 6, name: "Requests", link: "#", icon: <Icons name="help-circle" size="large"></Icons>},
+        {id: 7, name: "Report", link: "#", icon: <Icons name="document-text" size="large"></Icons>},
+        {id: 8, name: "Suspended", link: "#", icon: <Icons name="person-circle" size="large"></Icons>},        
+    ]
+
+    const navBar = [
+        {name: "Notifications", link: "#", desc: "notifications", icon: <Icons name="notifications"></Icons>},
+        {name: "Logout", desc: "logout", icon: <Icons name="power"></Icons>},        
+    ]
+
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div>Dashboard</div>
+        )
+    }
+    else{
+        localStorage.clear()
+        navigate('/')
+    }
+
 }
 
 export default Dashboard
