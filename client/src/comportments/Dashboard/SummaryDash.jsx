851d45028b308cd73443ae3fa0658ee6b0966248
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 16:13:53 +0530

    Update SummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/SummaryDash.jsx
+++ client/src/comportments/Dashboard/SummaryDash.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const SummaryDash = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
   return (
     <div>SummaryDash</div>
   )
