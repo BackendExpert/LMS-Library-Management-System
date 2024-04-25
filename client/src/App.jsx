cd526f2e0a8e05f7768eff84ddc44d9a62f6ebcb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 12:43:20 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./comportments/HomePage/HomePage";
+import SignIn from "./comportments/SignInSignUp/SignIn";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}/>
+        <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   )
