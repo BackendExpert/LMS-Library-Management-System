666fd252cd5e0cbebbf96d24c7892bec6070d52a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 16:17:03 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashboard.jsx
+++ client/src/comportments/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import Icons from "@reacticons/ionicons"
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import  secureLocalStorage  from  "react-secure-storage"
+import SummaryDash from "./SummaryDash"
 
 
 const Dashboard = () => {
@@ -126,7 +127,7 @@ const Dashboard = () => {
                             </div>                      
                         </div>
                         {/* navbar ENd */}
-                        
+                        <SummaryDash />
                     </div>
             </div>
         </div>
