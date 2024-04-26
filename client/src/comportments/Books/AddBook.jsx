740e9ad1abfd051bfa5f9f103a4d04667204edd4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-26 11:30:39 +0530

    Update AddBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/AddBook.jsx
+++ client/src/comportments/Books/AddBook.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const AddBook = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    
   return (
     <div>AddBook</div>
   )
