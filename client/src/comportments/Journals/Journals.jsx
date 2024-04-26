ba8a916dbc482e0501f2e11b5d18550b51f75f8d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-26 11:42:35 +0530

    Update Journals.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Journals/Journals.jsx
+++ client/src/comportments/Journals/Journals.jsx
@@ -15,36 +15,36 @@ const Journals = () => {
         SetButtonValue(clickValue)   
     }
 
-    const BookData = [
-        {id: 1, btnValue: "Books", name: "Books", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-green-500'},
-        {id: 2, btnValue: "bkBorrow", name: "Books Borrowed", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-red-500'},
-        {id: 3, btnValue: "bkSelected", name: "Books Selected", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-yellow-500'},
-        {id: 4, btnValue: "add_book", name: "Add New Book", icon: <Icons name="add" size="large"></Icons>, style: 'text-white bg-green-500'},
+    const JournalsData = [
+        {id: 1, btnValue: "Journals", name: "Journals", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-green-500'},
+        {id: 2, btnValue: "JournalsBorrow", name: "Journals Borrowed", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-red-500'},
+        {id: 3, btnValue: "JournalsSelected", name: "Journals Selected", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-yellow-500'},
+        {id: 4, btnValue: "add_Journals", name: "Add New Journals", icon: <Icons name="add" size="large"></Icons>, style: 'text-white bg-green-500'},
     ]
     if(RoleUser === "SuperAdmin"){
         return (
             <div className="py-4 px-4">
                 <div className="">
-                    <h1 className="px-4 py-2 text-xl font-semibold">Books</h1>
+                    <h1 className="px-4 py-2 text-xl font-semibold">Journals</h1>
                 </div>
                 <div className="">
                     <div className="lg:grid grid-cols-4 gap-4">
                         {
-                            BookData.map((Book) => {
-                                if(Book.id !== 4){
+                            JournalsData.map((Journal) => {
+                                if(Journal.id !== 4){
                                     return (
-                                        <div onClick={() => HeadleButtonClick(Book.btnValue)} className={`cursor-pointer text-center shadow-md bg-white border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Book.style}`}>                                       
-                                            <p className="font-semibold text-xl">{Book.icon}</p>   
-                                            <p className="font-semibold pl-2 pt-2">{Book.name}</p>
-                                            <p className="font-semibold text-3xl pl-2 pt-1">{Book.value}</p>
+                                        <div onClick={() => HeadleButtonClick(Journal.btnValue)} className={`cursor-pointer text-center shadow-md bg-white border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Journal.style}`}>                                       
+                                            <p className="font-semibold text-xl">{Journal.icon}</p>   
+                                            <p className="font-semibold pl-2 pt-2">{Journal.name}</p>
+                                            <p className="font-semibold text-3xl pl-2 pt-1">{Journal.value}</p>
                                         </div>  
                                     )
                                 }
                                 else{
                                     return (
-                                        <div onClick={() => HeadleButtonClick(Book.btnValue)} className={`cursor-pointer text-center shadow-md border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Book.style}`}>                                       
-                                            <p className="font-bold text-xl">{Book.icon}</p>   
-                                            <p className="font-semibold pl-2 pt-2">{Book.name}</p>
+                                        <div onClick={() => HeadleButtonClick(Journal.btnValue)} className={`cursor-pointer text-center shadow-md border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Journal.style}`}>                                       
+                                            <p className="font-bold text-xl">{Journal.icon}</p>   
+                                            <p className="font-semibold pl-2 pt-2">{Journal.name}</p>
                                         </div>  
                                     )
                                 }
