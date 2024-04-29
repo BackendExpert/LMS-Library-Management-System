d471fe77fdb176fa4645fd8a35bc72436ec74bbe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 10:31:32 +0530

    Update Thesis.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Thesis/Thesis.jsx
+++ client/src/comportments/Thesis/Thesis.jsx
@@ -22,9 +22,57 @@ const Thesis = () => {
         {id: 4, btnValue: "add_Thesis", name: "Add New Thesis", icon: <Icons name="add" size="large"></Icons>, style: 'text-white bg-green-500'},
     ]
 
-  return (
-    <div>Thesis</div>
-  )
+    if(RoleUser === "SuperAdmin"){
+        return (
+            <div className="py-4 px-4">
+                <div className="">
+                    <h1 className="px-4 py-2 text-xl font-semibold">Thesis</h1>
+                </div>
+                <div className="">
+                    <div className="lg:grid grid-cols-4 gap-4">
+                        {
+                            ThesisData.map((Thesis) => {
+                                if(Thesis.id !== 4){
+                                    return (
+                                        <div onClick={() => HeadleButtonClick(Thesis.btnValue)} className={`cursor-pointer text-center shadow-md bg-white border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Thesis.style}`}>                                       
+                                            <p className="font-semibold text-xl">{Thesis.icon}</p>   
+                                            <p className="font-semibold pl-2 pt-2">{Thesis.name}</p>
+                                            <p className="font-semibold text-3xl pl-2 pt-1">{Thesis.value}</p>
+                                        </div>  
+                                    )
+                                }
+                                else{
+                                    return (
+                                        <div onClick={() => HeadleButtonClick(Thesis.btnValue)} className={`cursor-pointer text-center shadow-md border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Thesis.style}`}>                                       
+                                            <p className="font-bold text-xl">{Thesis.icon}</p>   
+                                            <p className="font-semibold pl-2 pt-2">{Thesis.name}</p>
+                                        </div>  
+                                    )
+                                }
+                            })
+                        }
+                    </div>
+                </div>
+                {/* <p>{buttonValue}</p> */}
+                {/* {
+                    (() => {
+                        if(buttonValue === "add_Thesis"){
+                            return (
+                                <AddMagazine />
+                            )
+                        }
+                    })()
+                } */}
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
+
 }
 
 export default Thesis
