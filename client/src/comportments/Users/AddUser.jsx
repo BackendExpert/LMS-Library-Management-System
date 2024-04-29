4665638c9bc13684a59aa33f73814b1a257f6ac8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 11:45:48 +0530

    Update AddUser.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/AddUser.jsx
+++ client/src/comportments/Users/AddUser.jsx
@@ -14,7 +14,9 @@ const AddUser = () => {
 
     if(RoleUser === "SuperAdmin"){
         return (
-            <div>AddUser</div>
+            <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+                <h1 className="font-semibold text-gray-500 text-xl">Add New Book</h1>
+            </div>
         )
     }
     else{
