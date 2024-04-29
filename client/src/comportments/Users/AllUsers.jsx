4dde12003eced101f713dadeedcf8f850a223fd4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 11:03:21 +0530

    Update AllUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/AllUsers.jsx
+++ client/src/comportments/Users/AllUsers.jsx
@@ -56,7 +56,7 @@ const AllUsers = () => {
                                                 {users.username}
                                             </th>
                                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                {users.email}
+                                                {users.Email}
                                             </th>
                                             <td class="px-6 py-4">
                                                 {users.Role}
