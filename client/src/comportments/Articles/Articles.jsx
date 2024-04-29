e3977d81ce1aa8eb5726003d6d87d503b72694a8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 10:18:27 +0530

    Update Articles.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Articles/Articles.jsx
+++ client/src/comportments/Articles/Articles.jsx
@@ -5,6 +5,15 @@ import  secureLocalStorage  from  "react-secure-storage"
 import CountUp from 'react-countup';
 
 const Articles = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
   return (
     <div>Articles</div>
   )
