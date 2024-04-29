9033a3ccd4550b2ad1e8833a83425ebae8b785a2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 10:29:30 +0530

    Update Thesis.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Thesis/Thesis.jsx
+++ client/src/comportments/Thesis/Thesis.jsx
@@ -14,6 +14,14 @@ const Thesis = () => {
     const HeadleButtonClick = (clickValue) => {
         SetButtonValue(clickValue)   
     }
+
+    const ThesisData = [
+        {id: 1, btnValue: "Thesis", name: "Thesis", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-green-500'},
+        {id: 2, btnValue: "ThesisBorrow", name: "Thesis Borrowed", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-red-500'},
+        {id: 3, btnValue: "ThesisSelected", name: "Thesis Selected", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-yellow-500'},
+        {id: 4, btnValue: "add_Thesis", name: "Add New Thesis", icon: <Icons name="add" size="large"></Icons>, style: 'text-white bg-green-500'},
+    ]
+
   return (
     <div>Thesis</div>
   )
