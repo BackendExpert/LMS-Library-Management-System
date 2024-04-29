5dfcda5d0cb8b40e5ede25a768a1fbbca3a08c4f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 15:09:04 +0530

    Update MyProfile.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyProfile.jsx
+++ client/src/comportments/MyProfile/MyProfile.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup';
 
 const MyProfile = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
   return (
-    <div>MyProfile</div>
+    <div className="py-4 px-4">
+        <div className="">
+            <h1 className="px-4 py-2 text-xl font-semibold">My Profile</h1>
+        </div>
+    </div>
   )
 }
 
