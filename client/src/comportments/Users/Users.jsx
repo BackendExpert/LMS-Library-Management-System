352987f4c6527c8759ccb3c046c61c7cdd61ff81
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 09:42:14 +0530

    Create Users.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportments/Users/Users.jsx
@@ -0,0 +1,13 @@
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup';
+
+const Users = () => {
+  return (
+    <div>Users</div>
+  )
+}
+
+export default Users
