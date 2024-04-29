6510072c4ba68f2ca465718fe1482ead0e68a766
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 10:28:52 +0530

    Update Thesis.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Thesis/Thesis.jsx
+++ client/src/comportments/Thesis/Thesis.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup';
 
 const Thesis = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
   return (
     <div>Thesis</div>
   )
