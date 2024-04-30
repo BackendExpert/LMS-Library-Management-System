3a4bffe7a31f649da36c7a4e12580f777efd9427
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-30 13:31:12 +0530

    Update CheckOTP.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/SignInSignUp/CheckOTP.jsx
+++ client/src/comportments/SignInSignUp/CheckOTP.jsx
@@ -8,9 +8,39 @@ import  secureLocalStorage  from  "react-secure-storage"
 const CheckOTP = () => {
     const Email = secureLocalStorage.getItem("Token1");
 
+    const [OTPNo, SetOTPNo] = useState({
+        otp: ''
+    })
+
+    const headleSubmit = (e) => {
+
+    }
+
   return (
-    <div>CheckOTP
-        {Email}
+    <div className='bg-gray-200 py-24'>
+        <div className='lg:mx-20 mx-8'>
+            <div className="lg:flex bg-white shadow-md rounded w-full h-auto">       
+            <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://c1.wallpaperflare.com/preview/245/572/46/hacking-cyber-hacker-crime.jpg)] bg-center bg-cover h-auto w-full'>
+                <div className="text-white font-semibold text-3xl text-center my-16">Verify OTP</div>                    
+            </div>         
+            <div className="lg:mx-5 mx-0 py-12 px-10 w-full">
+                    <form onSubmit={headleSubmit}>
+                        <div className="my-5">
+                            <label htmlFor="">OTP Number : </label>
+                            <input type="email" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Email for Reset Password'
+                            onChange={e => SetOTPNo({...OTPNo, otp:e.target.value})}/>
+                        </div>
+                        <div className="">
+                            <button type='submit' className='w-full bg-red-500 rounded py-4 px-8 text-white duration-500 hover:bg-red-600 hover:shadow-md'>Verify OTP</button>
+                        </div>
+                    </form>
+                    <p>Enter the OTP that you reseved in Email</p>
+                </div>
+            </div>
+            <div className="my-1 text-center">
+                &copy; Developed and Design by : National Institute of Fundamental Studies
+            </div>
+        </div>
     </div>
   )
 }
