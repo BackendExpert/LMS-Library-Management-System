106ef347512fde760ff2f72aa947786a51c47c5f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-30 09:56:14 +0530

    Create BorrowedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportments/Books/BorrowedBooks.jsx
@@ -0,0 +1,25 @@
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+
+const BorrowedBooks = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    if(RoleUser === "SuperAdmin"){
+        return (
+            <div>BorrowedBooks</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
+}
+
+export default BorrowedBooks
