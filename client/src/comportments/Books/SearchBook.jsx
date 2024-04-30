485e699246dd894156479a036ece5460fa51ae1a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-30 09:32:08 +0530

    Update SearchBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBook.jsx
+++ client/src/comportments/Books/SearchBook.jsx
@@ -9,10 +9,17 @@ const SearchBook = () => {
     const RoleUser = secureLocalStorage.getItem("Login1");
     const EmailUser = secureLocalStorage.getItem("login2");
 
-    
-  return (
-    <div>SearchBook</div>
-  )
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div>SearchBook</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default SearchBook
