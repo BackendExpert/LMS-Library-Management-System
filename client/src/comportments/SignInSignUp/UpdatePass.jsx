4ec1330d36884b0a7b4c244bfcd25eab0ed2ddea
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-02 11:08:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/SignInSignUp/UpdatePass.jsx
+++ client/src/comportments/SignInSignUp/UpdatePass.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState } from 'react'
+import MyIcons from '@reacticons/ionicons'
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import  secureLocalStorage  from  "react-secure-storage"
+
 
 const UpdatePass = () => {
+  const navigate = useNavigate()
+  const Email = secureLocalStorage.getItem("Token1");
+  
   return (
     <div>UpdatePass</div>
   )
