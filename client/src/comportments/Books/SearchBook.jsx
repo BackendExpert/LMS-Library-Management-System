ae31a65ff213d3a7fee63d208d155bda24b9e0ae
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-02 12:33:38 +0530

    Update SearchBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBook.jsx
+++ client/src/comportments/Books/SearchBook.jsx
@@ -14,7 +14,7 @@ const SearchBook = () => {
             <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
                 <h1 className="font-semibold text-gray-500 text-xl">Search Book</h1>
                 <Link>
-                    <button className="text-blue-500 font-semibold my-4 py-2 duration-500 hover:bg-blue-500">Search More Books</button>
+                    <button className="text-blue-500 font-semibold my-4 py-2 rounded px-8  duration-500 hover:ml-2 hover:text-white hover:bg-blue-500">Search More Books</button>
                 </Link>
             </div>
         )
