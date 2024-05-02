99ed8d141d434ea462270d0073057b8f626b210a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-02 11:40:43 +0530

    Update UpdatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/SignInSignUp/UpdatePass.jsx
+++ client/src/comportments/SignInSignUp/UpdatePass.jsx
@@ -16,9 +16,45 @@ const UpdatePass = () => {
     cnewPass: ''
   })
 
+  const headleSubmit = (e) => {
 
+  }
   return (
-    <div>UpdatePass</div>
+    <div className='bg-gray-200 py-24'>
+        <div className='lg:mx-20 mx-8'>
+            <div className="lg:flex bg-white shadow-md rounded w-full h-auto">
+            <div className="lg:mx-5 mx-0 py-12 px-10 w-full">
+                    <form onSubmit={headleSubmit}>
+                        <div className="my-5">
+                            <label htmlFor="">Email : </label>
+                            <input type="email" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Email'
+                            onChange={e => SetLoginData({...LoginData, email:e.target.value})}/>
+                        </div>
+                        <div className="my-5">
+                            <label htmlFor="">Password : </label>
+                            <input type="password" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Password'
+                            onChange={e => SetLoginData({...LoginData, password:e.target.value})}/>
+                        </div>
+                        <div className="my-5">
+                            <label htmlFor="">Password : </label>
+                            <input type="password" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Password'
+                            onChange={e => SetLoginData({...LoginData, password:e.target.value})}/>
+                        </div>
+                        <div className="">
+                            <button type='submit' className='w-full bg-blue-500 rounded py-4 px-8 text-white duration-500 hover:bg-blue-600 hover:shadow-md'>SignIn</button>
+                        </div>
+                    </form>
+                    <p>Don't have an Account ? <Link to={'/SignUp'}><span className='text-blue-500'>SignUp</span></Link> || <span className="text-blue-500"><Link to={'/ForgetPass'}>Forget Password</Link></span></p>
+                </div>
+                <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp10395058.jpg)] bg-center bg-cover h-auto w-full'>
+                    <div className="text-white font-semibold text-3xl text-center my-16">Welcome Back</div>                    
+                </div>
+            </div>
+            <div className="my-1 text-center">
+                &copy; Developed and Design by : National Institute of Fundamental Studies
+            </div>
+        </div>
+    </div>
   )
 }
 
