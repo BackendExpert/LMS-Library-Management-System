06997d113a8d2acea7c44de00247a55f9b7c9613
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 07:40:59 +0530

    Update Welcome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Welcome.jsx
+++ client/src/comportments/HomePage/Welcome.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import NIFSLogo from '../../assets/nifs_logo.png'
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div>
+        <div className="text-center text-white">
+            <p className="text-5xl lib-title">Welcome to Library <br /> of</p>
+            <p className="my-4 text-3xl">National Institute of Fundamental Studies</p>
+            <img src={NIFSLogo} alt="" className='lg:mx-16 mx-4 my-4'/>
+        </div>
+    </div>
   )
 }
 
