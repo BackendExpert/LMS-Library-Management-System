cf1a98980edc1efe5408fa9c0e67015c4211eb03
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 09:37:50 +0530

    Update AddBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/AddBook.jsx
+++ client/src/comportments/Books/AddBook.jsx
@@ -41,6 +41,14 @@ const AddBook = () => {
                                 <label htmlFor="">ISBN Number</label>
                                 <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter Description"/>
                             </div>
+                            <div className="my-2">
+                                <label htmlFor="">KeyWord 1</label>
+                                <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter KeyWord 1"/>
+                            </div>
+                            <div className="my-2">
+                                <label htmlFor="">KeyWord 2</label>
+                                <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter KeyWord 2"/>
+                            </div>
                         </div>
                     </form>
                 </div>
