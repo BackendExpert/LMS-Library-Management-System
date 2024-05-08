1a5d780494af747cfe668555366a9e9306dc62c8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 10:53:40 +0530

    Update BorrowRequests.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/BorrowRequests.jsx
+++ client/src/comportments/Books/BorrowRequests.jsx
@@ -5,6 +5,12 @@ import { Link, useNavigate } from "react-router-dom"
 import  secureLocalStorage  from  "react-secure-storage"
 
 const BorrowRequests = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+
   return (
     <div>BorrowRequests</div>
   )
