7c188874ad6f267b4700d6069d81569268301d75
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 15:34:09 +0530

    Update ViewAllBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/ViewAllBooks.jsx
+++ client/src/comportments/Books/ViewAllBooks.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const ViewAllBooks = () => {
+  const navigate = useNavigate() 
+  //curent login user
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
+  
   return (
     <div>ViewAllBooks</div>
   )
