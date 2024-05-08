d483e15b75161008899a403f40610eed14877f21
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 19:13:12 +0530

    Update ViewAllBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/ViewAllBooks.jsx
+++ client/src/comportments/Books/ViewAllBooks.jsx
@@ -60,78 +60,145 @@ const ViewAllBooks = () => {
           </div>
         </div>
       </div>
-      <div className="lg:grid grid-cols-3 gap-3">
+      <div className="">
         {
-          allBooks.map((Books) => {
+          (() => {
             if(buttonValue === 0){
               return (
-                <div className="bg-white py-4 px-10 rounded shadow-md lg:my-0 my-4">
-                  <div className="lg:flex">
-                    <div className="">
-                      <h1 className="font-semibold">{Books.BookTitle}</h1>
-                      <p className="my-3"><span className="font-semibold">ISBN : </span> <span className="text-red-500">{Books.ISBNNumber} </span></p>
-                      <p className="my-3"><span className="font-semibold">Class No : </span> {Books.ClassNo}</p>
-                      <p className="my-3"><span className="font-semibold">Authors : </span> {Books.AuthorEditor}, {Books.AuthorEditor2}</p>
-  
-                    </div>
-                    <div className="">
-                    <p className="mx-4">
-                      {
-                        (() => {
-                          if(Books.Status === "Available"){
-                            return (
-                              <span className="font-semibold pl-2 text-green-500">Available</span>
-                            )
-                          }
-                        })()
-                      }
-  
-                      {
-                        (() => {
-                          if(RoleUser !== null && EmailUser !== null){
-                            if(Books.Status === "Available"){
-                              return (
-                                <button className="my-2 mx-3 bg-yellow-500 text-white font-semibold rounded py-2 px-4 duration-500 hover:bg-yellow-600">Select Book</button>
-                              )
+                <div className="lg:grid grid-cols-3 gap-4">
+                {
+                  allBooks.map((Books, index) => {
+                    return (
+                      <div className="bg-white py-4 px-10 rounded shadow-md lg:my-0 my-4">
+                        <div className="lg:flex">
+                          <div className="">
+                            <h1 className="font-semibold">{Books.BookTitle}</h1>
+                            <p className="my-3"><span className="font-semibold">ISBN : </span> <span className="text-red-500">{Books.ISBNNumber} </span></p>
+                            <p className="my-3"><span className="font-semibold">Class No : </span> {Books.ClassNo}</p>
+                            <p className="my-3"><span className="font-semibold">Authors : </span> {Books.AuthorEditor}, {Books.AuthorEditor2}</p>
+                          </div>
+                        <div className="">
+                          <p className="mx-4">
+                            {
+                              (() => {
+                                if(Books.Status === "Available"){
+                                  return (
+                                    <span className="font-semibold pl-2 text-green-500">Available</span>
+                                  )
+                                }
+                              })()
                             }
-                          }
-                          else{
-                            return (
-                              <Link to={'/'}>
-                                <p className="text-blue-500">Please Login to System</p>
-                              </Link>
-                            )
-                          }
-  
-                        })()
-                      }
-  
-                      
-                    </p>
-  
-                    </div>
-                  </div>
-                      <p className="my-3"><span className="font-semibold">Description : </span> {Books.Discription}</p>
-                      <p className="my-3"><span className="font-semibold">Publisher : </span>{Books.Publisher}</p>
-                      <p className="my-3"><span className="font-semibold">Publish Year : </span>{Books.PubYear}</p>
-                      <p className="my-3"><span className="font-semibold">Publish Place : </span>{Books.PubPlace}</p>
-                  <div className="my-8">
-  
-                  </div>
-  
+    
+                            {
+                              (() => {
+                                if(RoleUser !== null && EmailUser !== null){
+                                  if(Books.Status === "Available"){
+                                    return (
+                                      <button className="my-2 mx-3 bg-yellow-500 text-white font-semibold rounded py-2 px-4 duration-500 hover:bg-yellow-600">Select Book</button>
+                                    )
+                                  }
+                                }
+                                else{
+                                  return (
+                                    <Link to={'/'}>
+                                      <p className="text-blue-500">Please Login to System</p>
+                                    </Link>
+                                  )
+                                }
+        
+                              })()
+                            }
+                          </p>    
+                        </div>
+                        </div>
+                          <p className="my-3"><span className="font-semibold">Description : </span> {Books.Discription}</p>
+                          <p className="my-3"><span className="font-semibold">Publisher : </span>{Books.Publisher}</p>
+                          <p className="my-3"><span className="font-semibold">Publish Year : </span>{Books.PubYear}</p>
+                          <p className="my-3"><span className="font-semibold">Publish Place : </span>{Books.PubPlace}</p>
+                        <div className="my-8">    
+                        </div>    
+                      </div>
+                    )
+                
+                  })
+                }
                 </div>
-              )
+              )              
             }
+
             else if(buttonValue === "listData"){
               return (
-                
+                <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+                  <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                              <th scope="col" class="px-6 py-3">
+                                Book Title
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                Class No
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                Authors 
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                ISBN Number
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                Publisher
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                Status
+                              </th>
+                              <th scope="col" class="px-6 py-3">
+                                Action
+                              </th>
+                            </tr>
+                        </thead>
+                    <tbody>
+                      {
+                        allBooks.map((Books, index) => {
+                          return (
+                            <tr key={index}>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {Books.BookTitle}
+                            </th>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {Books.ClassNo}
+                            </th>
+                            <td class="px-6 py-4">
+                                {Books.AuthorEditor}, {Books.AuthorEditor2}
+                            </td>
+                            <td class="px-6 py-4">
+                                {Books.ISBNNumber}
+                            </td>
+                            <td class="px-6 py-4">
+                                {Books.Publisher}
+                            </td>
+                            <td class="px-6 py-4">
+                                {
+                                    (() => {
+                                        if(Books.Status === "Available"){
+                                            return(
+                                                <span className="py-2 px-6 bg-green-500 text-white rounded">Available</span>
+                                            )
+                                        }
+                                    })()
+                                }
+                            </td>
+                        </tr>
+                          )
+                        })
+                      }                                 
+                    </tbody>
+                </table>
+            </div>  
+            </div>              
               )
             }
-
-          })
+          })()
         }
-
-
       </div>
     </div>
   )
