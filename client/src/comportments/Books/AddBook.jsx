7b9d32720b3fa3621778b2144258cc95754f76f3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 09:36:19 +0530

    Update AddBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/AddBook.jsx
+++ client/src/comportments/Books/AddBook.jsx
@@ -13,6 +13,29 @@ const AddBook = () => {
         return (
             <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
                 <h1 className="font-semibold text-gray-500 text-xl">Add New Book</h1>
+
+                <div className="my-5">
+                    <form>
+                        <div className="lg:grid grid-cols-3 gap-4">
+                            <div className="my-2">
+                                <label htmlFor="">Book Title</label>
+                                <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter Book Title"/>
+                            </div>
+                            <div className="my-2">
+                                <label htmlFor="">Class No</label>
+                                <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter Class No"/>
+                            </div>
+                            <div className="my-2">
+                                <label htmlFor="">Author 1</label>
+                                <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter Author 1"/>
+                            </div>
+                            <div className="my-2">
+                                <label htmlFor="">Author 2</label>
+                                <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter Author 2"/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
