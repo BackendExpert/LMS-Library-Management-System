938b04d387a518898036e0ca7837759a8c8a1661
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 18:54:53 +0530

    Update ViewAllBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/ViewAllBooks.jsx
+++ client/src/comportments/Books/ViewAllBooks.jsx
@@ -10,7 +10,7 @@ const ViewAllBooks = () => {
     const RoleUser = secureLocalStorage.getItem("Login1");
     const EmailUser = secureLocalStorage.getItem("login2");
 
-    const [buttonValue, SetButtonValue] = useState()
+    const [buttonValue, SetButtonValue] = useState(0)
     const HeadleButtonClick = (clickValue) => {
         SetButtonValue(clickValue)   
     }
@@ -54,7 +54,7 @@ const ViewAllBooks = () => {
         <div className="my-4">
           <div className="lg:visible invisible">
             <div className="flex">
-              <p onClick={() => HeadleButtonClick('gridData')} className="mx-2 text-gray-500 cursor-pointer duration-500 hover:text-gray-600 "><Icons name="grid" size="large"></Icons></p>
+              <p onClick={() => HeadleButtonClick(0)} className="mx-2 text-gray-500 cursor-pointer duration-500 hover:text-gray-600 "><Icons name="grid" size="large"></Icons></p>
               <p onClick={() => HeadleButtonClick('listData')} className="mx-2 text-gray-500 cursor-pointer duration-500 hover:text-gray-600 "><Icons name="list" size="large"></Icons></p>              
             </div>            
           </div>
@@ -63,63 +63,66 @@ const ViewAllBooks = () => {
       <div className="lg:grid grid-cols-3 gap-3">
         {
           allBooks.map((Books) => {
-            return (
-              <div className="bg-white py-4 px-10 rounded shadow-md lg:my-0 my-4">
-                <div className="lg:flex">
-                  <div className="">
-                    <h1 className="font-semibold">{Books.BookTitle}</h1>
-                    <p className="my-3"><span className="font-semibold">ISBN : </span> <span className="text-red-500">{Books.ISBNNumber} </span></p>
-                    <p className="my-3"><span className="font-semibold">Class No : </span> {Books.ClassNo}</p>
-                    <p className="my-3"><span className="font-semibold">Authors : </span> {Books.AuthorEditor}, {Books.AuthorEditor2}</p>
-
-                  </div>
-                  <div className="">
-                  <p className="mx-4">
-                    {
-                      (() => {
-                        if(Books.Status === "Available"){
-                          return (
-                            <span className="font-semibold pl-2 text-green-500">Available</span>
-                          )
-                        }
-                      })()
-                    }
-
-                    {
-                      (() => {
-                        if(RoleUser !== null && EmailUser !== null){
+            if(buttonValue === 0){
+              return (
+                <div className="bg-white py-4 px-10 rounded shadow-md lg:my-0 my-4">
+                  <div className="lg:flex">
+                    <div className="">
+                      <h1 className="font-semibold">{Books.BookTitle}</h1>
+                      <p className="my-3"><span className="font-semibold">ISBN : </span> <span className="text-red-500">{Books.ISBNNumber} </span></p>
+                      <p className="my-3"><span className="font-semibold">Class No : </span> {Books.ClassNo}</p>
+                      <p className="my-3"><span className="font-semibold">Authors : </span> {Books.AuthorEditor}, {Books.AuthorEditor2}</p>
+  
+                    </div>
+                    <div className="">
+                    <p className="mx-4">
+                      {
+                        (() => {
                           if(Books.Status === "Available"){
                             return (
-                              <button className="my-2 mx-3 bg-yellow-500 text-white font-semibold rounded py-2 px-4 duration-500 hover:bg-yellow-600">Select Book</button>
+                              <span className="font-semibold pl-2 text-green-500">Available</span>
                             )
                           }
-                        }
-                        else{
-                          return (
-                            <Link to={'/'}>
-                              <p className="text-blue-500">Please Login to System</p>
-                            </Link>
-                          )
-                        }
-
-                      })()
-                    }
-
-                    
-                  </p>
-
+                        })()
+                      }
+  
+                      {
+                        (() => {
+                          if(RoleUser !== null && EmailUser !== null){
+                            if(Books.Status === "Available"){
+                              return (
+                                <button className="my-2 mx-3 bg-yellow-500 text-white font-semibold rounded py-2 px-4 duration-500 hover:bg-yellow-600">Select Book</button>
+                              )
+                            }
+                          }
+                          else{
+                            return (
+                              <Link to={'/'}>
+                                <p className="text-blue-500">Please Login to System</p>
+                              </Link>
+                            )
+                          }
+  
+                        })()
+                      }
+  
+                      
+                    </p>
+  
+                    </div>
                   </div>
+                      <p className="my-3"><span className="font-semibold">Description : </span> {Books.Discription}</p>
+                      <p className="my-3"><span className="font-semibold">Publisher : </span>{Books.Publisher}</p>
+                      <p className="my-3"><span className="font-semibold">Publish Year : </span>{Books.PubYear}</p>
+                      <p className="my-3"><span className="font-semibold">Publish Place : </span>{Books.PubPlace}</p>
+                  <div className="my-8">
+  
+                  </div>
+  
                 </div>
-                    <p className="my-3"><span className="font-semibold">Description : </span> {Books.Discription}</p>
-                    <p className="my-3"><span className="font-semibold">Publisher : </span>{Books.Publisher}</p>
-                    <p className="my-3"><span className="font-semibold">Publish Year : </span>{Books.PubYear}</p>
-                    <p className="my-3"><span className="font-semibold">Publish Place : </span>{Books.PubPlace}</p>
-                <div className="my-8">
+              )
+            }
 
-                </div>
-
-              </div>
-            )
           })
         }
 
