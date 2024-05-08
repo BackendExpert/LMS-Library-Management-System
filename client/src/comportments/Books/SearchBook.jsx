e93d0533837168017b297db2ca7f836e872af759
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 15:06:42 +0530

    Update SearchBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBook.jsx
+++ client/src/comportments/Books/SearchBook.jsx
@@ -93,12 +93,13 @@ const SearchBook = () => {
                     </form>
                 </div>
             </div>
-            <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
-                <div className="">
+
                                 {
                                     (() => {
                                         if(isFormSubmited === true){
                                             return (
+                                                <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+                                                <div className="">
                                                 <div class="relative overflow-x-auto">
                                                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -165,6 +166,8 @@ const SearchBook = () => {
                                                     </tbody>
                                                 </table>
                                             </div>
+                                            </div>
+                                        </div>
                                             )
                                         }        
                                         else{
@@ -175,8 +178,7 @@ const SearchBook = () => {
                                     })()
                                 }  
                         
-                    </div>
-            </div>
+
 
             </div>
         )
