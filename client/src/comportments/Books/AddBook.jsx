23d275877c1286d89da05b6a377d3907778a3834
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 09:44:26 +0530

    Update AddBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/AddBook.jsx
+++ client/src/comportments/Books/AddBook.jsx
@@ -9,13 +9,31 @@ const AddBook = () => {
     const RoleUser = secureLocalStorage.getItem("Login1");
     const EmailUser = secureLocalStorage.getItem("login2");
 
+    const [BookData, SetBookData] = useState({
+        title: '',
+        classNo: '',
+        Author1: '',
+        Author2: '',
+        Description: '',
+        isbnNo: '',
+        KeyWord1: '',
+        KeyWord2: '',
+        Publisher: '',
+        pubYear: '',
+        pubPlace: ''
+    })
+
+    const headleSubmit = (e) => {
+        
+    }
+
     if(RoleUser === "SuperAdmin"){
         return (
             <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
                 <h1 className="font-semibold text-gray-500 text-xl">Add New Book</h1>
 
                 <div className="my-5">
-                    <form>
+                    <form onSubmit={headleSubmit}>
                         <div className="lg:grid grid-cols-3 gap-4">
                             <div className="my-2">
                                 <label htmlFor="">Book Title</label>
@@ -61,6 +79,9 @@ const AddBook = () => {
                                 <label htmlFor="">Publish Place</label>
                                 <input type="text" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" required placeholder="Enter Publish Place"/>
                             </div>
+                        </div>
+                        <div className="">
+                            <button type="submit" className="py-4 px-8 bg-green-500 rounded text-white duration-500 hover:ml-2">Add New Book</button>
                         </div>
                     </form>
                 </div>
