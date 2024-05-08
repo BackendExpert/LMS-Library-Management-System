08d37c7c89713c0a5b5a900fd686e5a4b5ffb9da
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 11:29:01 +0530

    Update SearchBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBook.jsx
+++ client/src/comportments/Books/SearchBook.jsx
@@ -29,7 +29,7 @@ const SearchBook = () => {
         e.preventDefault();
         // alert("hellow Work")
 
-        axios.get(`http://localhost:8081/SearchBook?searchData=${SearchBook}`)
+        axios.get('http://localhost:8081/SearchBook', { params : {SearchBookData} })
         .then(res => {
             if(res.data.Status === "Success"){
                 SetSearchBookData(res.data)
