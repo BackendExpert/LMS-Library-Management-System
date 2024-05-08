66900ffcd8ad6c972d52b8ce02e009610b86cbd9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 15:08:20 +0530

    Update Books.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/Books.jsx
+++ client/src/comportments/Books/Books.jsx
@@ -54,23 +54,37 @@ const Books = () => {
                     <div className="lg:grid grid-cols-4 gap-4">
                         {
                             BookData.map((Book) => {
-                                if(Book.id !== 5){
-                                    return (
-                                        <div onClick={() => HeadleButtonClick(Book.btnValue)} className={`cursor-pointer text-center shadow-md bg-white border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Book.style}`}>                                       
-                                            <p className="font-semibold text-xl">{Book.icon}</p>   
-                                            <p className="font-semibold pl-2 pt-2">{Book.name}</p>
-                                            <p className="font-semibold text-3xl pl-2 pt-1">{Book.value}</p>
-                                        </div>  
-                                    )
+                                if(RoleUser === "SuperAdmin"){
+                                    if(Book.id !== 5){
+                                        return (
+                                            <div onClick={() => HeadleButtonClick(Book.btnValue)} className={`cursor-pointer text-center shadow-md bg-white border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Book.style}`}>                                       
+                                                <p className="font-semibold text-xl">{Book.icon}</p>   
+                                                <p className="font-semibold pl-2 pt-2">{Book.name}</p>
+                                                <p className="font-semibold text-3xl pl-2 pt-1">{Book.value}</p>
+                                            </div>  
+                                        )
+                                    }
+                                    else{
+                                        return (
+                                            <div onClick={() => HeadleButtonClick(Book.btnValue)} className={`cursor-pointer text-center shadow-md border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Book.style}`}>                                       
+                                                <p className="font-bold text-xl">{Book.icon}</p>   
+                                                <p className="font-semibold pl-2 pt-2">{Book.name}</p>
+                                            </div>  
+                                        )
+                                    }
                                 }
-                                else{
-                                    return (
-                                        <div onClick={() => HeadleButtonClick(Book.btnValue)} className={`cursor-pointer text-center shadow-md border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Book.style}`}>                                       
-                                            <p className="font-bold text-xl">{Book.icon}</p>   
-                                            <p className="font-semibold pl-2 pt-2">{Book.name}</p>
-                                        </div>  
-                                    )
+                                else if(RoleUser === "user"){
+                                    if(Book.id === 1){
+                                        return (
+                                            <div onClick={() => HeadleButtonClick(Book.btnValue)} className={`cursor-pointer text-center shadow-md bg-white border-2 border-gray-200 rounded-2xl py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${Book.style}`}>                                       
+                                                <p className="font-semibold text-xl">{Book.icon}</p>   
+                                                <p className="font-semibold pl-2 pt-2">{Book.name}</p>
+                                                <p className="font-semibold text-3xl pl-2 pt-1">{Book.value}</p>
+                                            </div>  
+                                        )
+                                    }
                                 }
+
                             })
                         }
                     </div>
