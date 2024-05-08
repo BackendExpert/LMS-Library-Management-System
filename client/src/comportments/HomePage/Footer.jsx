ef7d6dcdf2cb2eeab67924fbad00bfe7458b296f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 19:57:36 +0530

    Update Footer.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Footer.jsx
+++ client/src/comportments/HomePage/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <div className="lg:flex">
             <div className="lg:px-4 px-0 w-full">
                 <h1 className="text-xl font-semibold font-semibold text-gray-500 my-4 ">About us</h1>
-                <img src="" alt="" />
+                <img src={NIFSLogo} alt="" />
                 <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate iste suscipit nesciunt, dolore inventore provident fuga incidunt quibusdam nobis tenetur culpa at! Facilis, rerum illo? Pariatur aperiam error corporis fuga.</p>
             </div>
             <div className="lg:px-4 px-0 w-full">
