435f0e613103f024de1bf3adeb71f19a0a4208f2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 14:00:01 +0530

    Update SearchBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBook.jsx
+++ client/src/comportments/Books/SearchBook.jsx
@@ -95,7 +95,53 @@ const SearchBook = () => {
                         (() => {
                             if(isFormSubmited === true){
                                 return (
-                                    
+                                    <div class="relative overflow-x-auto">
+                                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                            <tr>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Book Title
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Class No
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Author 1
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Author 2
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Description
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    ISBN Number
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Keywords
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Publisher
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Publish Year
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Publish Place
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Status
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Action
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                    </table>
+                                </div>
                                 )
                             }        
                             else{
