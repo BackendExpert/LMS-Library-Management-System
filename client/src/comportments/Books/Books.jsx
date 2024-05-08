dd506bea94c6ddb4e970d61bde350c3da0e669c7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 10:40:49 +0530

    Update Books.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/Books.jsx
+++ client/src/comportments/Books/Books.jsx
@@ -19,6 +19,18 @@ const Books = () => {
         SetButtonValue(clickValue)   
     }
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const BKCount = await axios.get('http://localhost:8081/BooksCount');
+                SetCoutBooks(BKCount.data.BKs);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, [])
+
     const BookData = [
         {id: 1, btnValue: "Books", name: "Books", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-green-500'},
         {id: 2, btnValue: "bkBorrow", name: "Books Borrowed", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: 'text-red-500'},
