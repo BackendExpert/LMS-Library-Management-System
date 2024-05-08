5cd7b103251bae9d136c9fcb548a16eb347465fa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 10:58:12 +0530

    Update SelectedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SelectedBooks.jsx
+++ client/src/comportments/Books/SelectedBooks.jsx
@@ -14,6 +14,7 @@ const SelectedBooks = () => {
         return (
             <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
                 <h1 className="font-semibold text-gray-500 text-xl">Selected Books</h1>
+
             </div>
         )
     }
