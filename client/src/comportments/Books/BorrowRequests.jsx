c8afaebc3862571f89eff2bd83b039360b2c0881
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-08 10:55:15 +0530

    Update BorrowRequests.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/BorrowRequests.jsx
+++ client/src/comportments/Books/BorrowRequests.jsx
@@ -12,7 +12,9 @@ const BorrowRequests = () => {
 
     if(RoleUser === "SuperAdmin"){
         return (
-            <div>BorrowRequests</div>
+            <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+                <h1 className="font-semibold text-gray-500 text-xl">Borrow Requests</h1>
+            </div>
         )
     }
     else{
