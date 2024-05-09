af008b1674b67e22108ddda2e1b032e9a2f6ab68
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-09 09:53:38 +0530

    Update SearchBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBook.jsx
+++ client/src/comportments/Books/SearchBook.jsx
@@ -155,7 +155,7 @@ const SearchBook = () => {
                                                         <tbody>
                                                             {
                                                                 SearchBookData.map((BookData, index) => {
-                                                                    if(BookData.Status !== "Disabled"){
+                                                                    if(BookData.Status !== "Disabled" && RoleUser === "user"){
                                                                         return (                                                                        
                                                                             <tr key={index}>
                                                                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
