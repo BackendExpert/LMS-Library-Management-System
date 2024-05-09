147e11262b60f83365ecc30e2d73af4e89e66eda
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-09 09:40:41 +0530

    Update SearchBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBook.jsx
+++ client/src/comportments/Books/SearchBook.jsx
@@ -176,7 +176,7 @@ const SearchBook = () => {
                                                                                         if(RoleUser === "SuperAdmin"){
                                                                                             return (
                                                                                                 <div className="">
-                                                                                                  <button className="bg-red-500 text-white rounded py-2 px-6 duration-500 hover:bg-red-600">Request to Borrow</button>
+                                                                                                  <button className="bg-red-500 text-white rounded py-2 px-6 duration-500 hover:bg-red-600">Disabled</button>
                                                                                                 </div>
                                                                                             )
                                                                                         }
