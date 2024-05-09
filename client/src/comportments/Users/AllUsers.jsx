090ab3577139dc7d40532ddc1c0853c6aecc5b28
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-09 12:45:21 +0530

    Update AllUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/AllUsers.jsx
+++ client/src/comportments/Users/AllUsers.jsx
@@ -33,7 +33,7 @@ const AllUsers = () => {
         .then(res => {
             if(res.data.Status === "Success"){
                 alert("User Request has been Rejected")
-                SetButtonValue('All_Users')
+                window.location.reload()
             }
             else{
                 alert(res.data.Error)
