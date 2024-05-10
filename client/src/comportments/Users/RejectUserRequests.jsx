4f28861e013dfa20ce8b5cba94de95a49086c077
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 11:45:48 +0530

    Update RejectUserRequests.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/RejectUserRequests.jsx
+++ client/src/comportments/Users/RejectUserRequests.jsx
@@ -54,6 +54,12 @@ const RejectUserRequests = () => {
                                                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                         {rejectuser.ID}
                                                     </th>
+                                                    <td class="px-6 py-4">
+                                                        {rejectuser.Email}
+                                                    </td>
+                                                    <td class="px-6 py-4">
+                                                        {rejectuser.reject_at}
+                                                    </td>
                                                 </tr>
                                             )
                                         })
