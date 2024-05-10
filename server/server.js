bedb89bd36d1f4d6fed0d9469a8dee27e6223e7e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 15:50:22 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -914,7 +914,7 @@ app.post('/RejectAccount/:id', (req, res) => {
 
 app.post('/BorrowBook/:id', (req, res) => {
     const BookID = req.params.id
-    console.log(BookID, req.body)
+    console.log(BookID)
     // const sql = "UPDATE book SET Status = ? WHERE ISBNNumber = ?"
     // const status = "Requested"
     
