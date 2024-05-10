c9bb050e1c136332fdd3cdf1e2f0e4666123436e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 11:19:23 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/SignInSignUp/SignIn.jsx
+++ client/src/comportments/SignInSignUp/SignIn.jsx
@@ -69,21 +69,21 @@ const SignIn = () => {
     <div className='bg-none py-8 rounded text-white'>
         <div className=''>
             <div className="mx-0 py-12 px-10 w-full">
-                    <h1 className="text-gray-500 font-semibold">WELCOME TO LIBRARY</h1>
-                    <h1 className="">Create New Account</h1>
+                    <h1 className="text-gray-500 font-semibold">WELCOME BACK</h1>
+                    <h1 className="text-3xl my-4 lib-title"></h1>
                     <form onSubmit={headleSubmit}>
                         <div className="my-5">
                             <label htmlFor="">Email : </label>
-                            <input type="email" name="" id="" className='bg-transparent w-full border border-gray-300 my-2 h-12 rounded-xl pl-4 shadow-md' required placeholder='Enter Email'
+                            <input type="email" name="" id="" className='w-full bg-[#334155] my-2 h-14 rounded-xl pl-4 shadow-md duration-500' required placeholder='Enter Email'
                             onChange={e => SetLoginData({...LoginData, email:e.target.value})}/>
                         </div>
                         <div className="my-5">
                             <label htmlFor="">Password : </label>
-                            <input type="password" name="" id="" className='bg-transparent w-full border border-gray-300 my-2 h-12 rounded-xl pl-4 shadow-md' required placeholder='Enter Password'
+                            <input type="password" name="" id="" className=' w-full bg-[#334155] my-2 h-14 rounded-xl pl-4 shadow-md duration-500' required placeholder='Enter Password'
                             onChange={e => SetLoginData({...LoginData, password:e.target.value})}/>
                         </div>
                         <div className="">
-                            <button type='submit' className='w-full bg-blue-500 rounded py-4 px-8 text-white duration-500 hover:bg-blue-600 hover:shadow-md'>SignIn</button>
+                            <button type='submit' className='w-full bg-blue-500 rounded-full py-4 px-8 text-white duration-500 hover:bg-blue-600 hover:shadow-md'>SignIn</button>
                         </div>
                     </form>
                     <span className="text-blue-500"><Link to={'/ForgetPass'}>Forget Password</Link></span>
