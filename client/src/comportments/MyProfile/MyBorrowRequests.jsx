ce15df6910735ba49bd0ea1a4ae5a7d7984756fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 19:16:39 +0530

    Update MyBorrowRequests.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyBorrowRequests.jsx
+++ client/src/comportments/MyProfile/MyBorrowRequests.jsx
@@ -21,6 +21,9 @@ const MyBorrowRequests = () => {
             <div>
                 <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
                     <h1 className="font-semibold text-gray-500 text-xl">My Borrow Requests</h1>
+
+                    
+
                 </div>
             </div>
         )
