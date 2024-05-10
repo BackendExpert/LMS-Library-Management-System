65ed9f0011e1fb44942024deac5565e4e1780bf9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 11:34:39 +0530

    Update RejectUserRequests.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/RejectUserRequests.jsx
+++ client/src/comportments/Users/RejectUserRequests.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const RejectUserRequests = () => {
   return (
-    <div>RejectUserRequests</div>
+    <div>
+        <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
+            <h1 className='text-gray-500 text-2xl font-semibold my-4'>Rejected User Requests</h1>
+        </div>
+    </div>
   )
 }
 
