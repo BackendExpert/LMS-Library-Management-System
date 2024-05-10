5154651c2d1dddf67584431d27e929dfae2c6ffa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 11:20:21 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/SignInSignUp/SignUp.jsx
+++ client/src/comportments/SignInSignUp/SignUp.jsx
@@ -31,31 +31,27 @@ const SignUp = () => {
     }
 
   return (
-    <div className='bg-gray-200 py-12'>
+    <div className='bg-none-200 py-12 text-white'>
         <div className='lg:mx-10'>
-                <div onClick={headleBack} className='mb-1 mx-4 flex duration-500 hover:mx-2 cursor-pointer'>
-                    <span className='mt-[1px] mx-2'><MyIcons name='arrow-back'></MyIcons></span>
-                    <p className="">Back to Home</p>
-                </div>
                 <div className="py-2 px-10 w-full">
                     <form onSubmit={headleSubmit}>
                         <div className="my-5">
                             <label htmlFor="">Username : </label>
-                            <input type="text" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Username'
+                            <input type="text" name="" id="" className='w-full bg-[#334155] my-2 h-14 rounded-xl pl-4 shadow-md duration-500' required placeholder='Enter Username'
                             onChange={e => SetUserData({...UserData, username:e.target.value})}/>
                         </div>
                         <div className="my-5">
                             <label htmlFor="">Email : </label>
-                            <input type="email" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Email'
+                            <input type="email" name="" id="" className='w-full bg-[#334155] my-2 h-14 rounded-xl pl-4 shadow-md duration-500' required placeholder='Enter Email'
                             onChange={e => SetUserData({...UserData, email:e.target.value})}/>
                         </div>
                         <div className="my-5">
                             <label htmlFor="">Password : </label>
-                            <input type="password" name="" id="" className='w-full border border-gray-300 my-2 h-12 rounded pl-4 shadow-md' required placeholder='Enter Password'
+                            <input type="password" name="" id="" className='w-full bg-[#334155] my-2 h-14 rounded-xl pl-4 shadow-md duration-500' required placeholder='Enter Password'
                             onChange={e => SetUserData({...UserData, password:e.target.value})}/>
                         </div>
                         <div className="">
-                            <button type='submit' className='w-full bg-blue-500 rounded py-4 px-8 text-white duration-500 hover:bg-blue-600 hover:shadow-md'>SignUp</button>
+                            <button type='submit' className='w-full bg-blue-500 rounded-full py-4 px-8 text-white duration-500 hover:bg-blue-600 hover:shadow-md'>SignUp</button>
                         </div>
                     </form>
                 </div>
