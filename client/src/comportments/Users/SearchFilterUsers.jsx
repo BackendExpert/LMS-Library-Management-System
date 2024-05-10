d072164af2bd7e89d66b3faedf9b639438689591
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 13:54:02 +0530

    Update SearchFilterUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/SearchFilterUsers.jsx
+++ client/src/comportments/Users/SearchFilterUsers.jsx
@@ -113,23 +113,7 @@ const SearchFilterUsers = () => {
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            {
-                                                RejectUsers.map((rejectuser, index) => {
-                                                    return (
-                                                        <tr key={index}>
-                                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                                {rejectuser.ID}
-                                                            </th>
-                                                            <td class="px-6 py-4">
-                                                                {rejectuser.Email}
-                                                            </td>
-                                                            <td class="px-6 py-4">
-                                                                {rejectuser.reject_at}
-                                                            </td>
-                                                        </tr>
-                                                    )
-                                                })
-                                            }
+
                                         </tbody>
                                     </table>
                                 </div>
