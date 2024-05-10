5a9cbb960a9b635f069fb325218c9bc936bdace3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 12:48:01 +0530

    Update SearchFilterUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/SearchFilterUsers.jsx
+++ client/src/comportments/Users/SearchFilterUsers.jsx
@@ -20,9 +20,11 @@ const SearchFilterUsers = () => {
             <div>
                 <div className="my-4">
                     <form>
-                        <div className="my-2">
-                            <label htmlFor="">Email Address</label>
-                            <input type="email" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" placeholder="Enter Email Address"/>
+                        <div className="lg:grid grid-cols-3 gap-4">
+                            <div className="my-2">
+                                <label htmlFor="">Email Address</label>
+                                <input type="email" name="" id="" className="w-full h-12 rounded bg-gray-200 pl-2 my-2" placeholder="Enter Email Address"/>
+                            </div>
                         </div>
                     </form>
                 </div>
