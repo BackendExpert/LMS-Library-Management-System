aa1de56c0f01fd007b3425412b21e214fc964648
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 10:55:34 +0530

    Update HomeQuote.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomeQuote.jsx
+++ client/src/comportments/HomePage/HomeQuote.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const HomeQuote = () => {
   return (
     <div className='my-16'>
-        <p className="text-white lg:text-3xl text-2xl lib-title text-center">
+        <h1 className="lib-title text-center text-white text-5xl my-8">Welcome to Library</h1>
+        <p className="text-white lg:text-xl text-center">
             "A library is a house of hope. It's a place where we all, whatever our situation, can feed our ideas and develop our dreams"
         </p>
         <p className="text-white my-8 text-xl text-righ text-center">
