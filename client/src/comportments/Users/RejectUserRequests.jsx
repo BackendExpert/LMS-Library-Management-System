7d3e062a1704b9a28cfbffda7a64dd990e629b96
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 11:35:37 +0530

    Update RejectUserRequests.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Users/RejectUserRequests.jsx
+++ client/src/comportments/Users/RejectUserRequests.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup';
+import axios from "axios";
 
 const RejectUserRequests = () => {
-  return (
-    <div>
-        <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
-            <h1 className='text-gray-500 text-2xl font-semibold my-4'>Rejected User Requests</h1>
-        </div>
-    </div>
-  )
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
+    if(RoleUser === "SuperAdmin"){
+        return (
+            <div>
+                <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
+                    <h1 className='text-gray-500 text-2xl font-semibold my-4'>Rejected User Requests</h1>
+                </div>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default RejectUserRequests
