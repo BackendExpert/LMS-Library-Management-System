068073c077c380da153902c1ef9585707901f8c7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 15:39:43 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -914,7 +914,7 @@ app.post('/RejectAccount/:id', (req, res) => {
 
 app.post('/BorrowBook/:id', (req, res) => {
     const userEmail = req.params.id
-    console.log(userEmail, req.body)
+    console.log(req.body)
 
 })
 
