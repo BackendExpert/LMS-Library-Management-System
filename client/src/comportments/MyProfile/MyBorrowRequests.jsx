0ac879447dac87d0598e1dff8a2e37def92ed9c6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 19:12:55 +0530

    Create MyBorrowRequests.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportments/MyProfile/MyBorrowRequests.jsx
@@ -0,0 +1,24 @@
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup';
+
+
+const MyBorrowRequests = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
+  return (
+    <div>MyBorrowRequests</div>
+  )
+}
+
+export default MyBorrowRequests
