d0aac2bbc474ad1d7bf6fd8b8c58d929e86bf5b5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-10 16:14:05 +0530

    Update ViewAllBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/ViewAllBooks.jsx
+++ client/src/comportments/Books/ViewAllBooks.jsx
@@ -142,9 +142,17 @@ const ViewAllBooks = () => {
                                 (() => {
                                   if(RoleUser !== null && EmailUser !== null){
                                     if(Books.Status === "Available"){
-                                      return (
-                                        <button onClick={() => headleBorrowBook(Books.ISBNNumber)} className="my-2 mx-3 bg-blue-500 text-white font-semibold rounded py-2 px-4 duration-500 hover:bg-blue-600">Request to Borrow</button>
-                                      )
+                                      if(myBkRequest <= 3){
+                                        return (
+                                          <button onClick={() => headleBorrowBook(Books.ISBNNumber)} className="my-2 mx-3 bg-blue-500 text-white font-semibold rounded py-2 px-4 duration-500 hover:bg-blue-600">Request to Borrow</button>
+                                        )
+                                      }
+                                      else{
+                                        return (
+                                          <div className=""></div>
+                                        )
+                                      }
+
                                     }
                                   }
                                   else{
