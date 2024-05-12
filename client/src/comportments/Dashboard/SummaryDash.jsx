4bd89ae5e97f85b62c10a044861a33bca1cd6e05
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-12 08:36:25 +0530

    Update SummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/SummaryDash.jsx
+++ client/src/comportments/Dashboard/SummaryDash.jsx
@@ -79,7 +79,6 @@ const SummaryDash = () => {
         {id: 7, btnvalue: "", name: "My Borrowed", link: "#", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: "text-green-500"},
         {id: 8, btnvalue: "Users", name: "Users", link: "#", value: <CountUp end={AllUserCount}/>, icon: <Icons name="people" size="large"></Icons>, style: "text-green-500"}, 
         {id: 9, btnvalue: "myRequests", name: "My Book Requests", link: "#", value: <CountUp end={MyBookReqeusts}/>, icon: <Icons name="help-circle" size="large"></Icons>, style: "text-blue-500"}, 
-        {id: 10, btnvalue: "borrowRequests", name: "Book Borrow Requests", link: "#", value: <CountUp end={BorrowRequests}/>, icon: <Icons name="book" size="large"></Icons>, style: "text-green-500"},               
     ]
 
     const [UserData, SetUserData] = useState([])
@@ -169,11 +168,7 @@ const SummaryDash = () => {
                                 <MyBorrowRequests />
                             )
                         }
-                        if(buttonValue === "borrowRequests"){
-                            return (
-                                <BorrowRequests />
-                            )
-                        }
+
                     })()
                 }
 
