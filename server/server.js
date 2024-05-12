04ee94de4be72c1b997f58f3be89efe303ef0d47
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-12 17:47:48 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1114,13 +1114,14 @@ app.post('/BorrowAcceptBook/:id', (req, res) => {
 
     const checkBook = "SELECT * FROM book_borrow_request WHERE bookISBN = ? && borrowEmail = ? && status = ?"
     const status = "Accept"
+    const borrower =  req.body.Email
 
-    connection.query(checkBook, [BookISBN, req.body.Email, status], (err, result) => {
+    connection.query(checkBook, [BookISBN, borrower, status], (err, result) => {
         if (err) throw err
 
         if(result){
             const sql = "UPDATE book_borrow_request SET status = ? WHERE bookISBN = ?"
-            const status = "Accept"
+            const status = "Borrowed"
 
             connection.query(sql, [status, BookISBN], (err, result) => {
                 if(err) {
