b8fad8d861884cefc753d298d469f96b0f07c300
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-13 11:22:26 +0530

    Update SearchBookRequest.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBookRequest.jsx
+++ client/src/comportments/Books/SearchBookRequest.jsx
@@ -12,7 +12,11 @@ const SearchBookRequest = () => {
 
     if(RoleUser !== "SuperAdmin") {
         return (
-            <div>SearchBookRequest</div>
+            <div className="">
+                <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+                    <h1 className="font-semibold text-gray-500 text-xl">Search Book</h1>
+                </div>
+            </div>
         )
     }
     else{
