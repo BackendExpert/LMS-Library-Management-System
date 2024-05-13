c426c8ae13e83a495c85d0ee68f18f0ed731f91d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-13 14:14:04 +0530

    Update BorrowedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/BorrowedBooks.jsx
+++ client/src/comportments/Books/BorrowedBooks.jsx
@@ -18,6 +18,42 @@ const BorrowedBooks = () => {
         return (
             <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
                 <h1 className="font-semibold text-gray-500 text-xl">Borrowed Books</h1>
+
+                {
+                    (() => {
+                        if(buttonValue === 0){
+                            return (
+                                <div class="relative overflow-x-auto my-8">
+                                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                            <tr>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Book ISBN Number
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Borrower Email
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Borrow at
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Status
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Action
+                                                </th>
+                                            </tr>
+            
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                    </table>
+                                </div>   
+                            )
+                        }
+                    })()
+                }
             </div>
         )
     }
