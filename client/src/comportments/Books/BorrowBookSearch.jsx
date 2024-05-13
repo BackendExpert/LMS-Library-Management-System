7f25c79b31f81f69083d201811f145dc01df38d1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-13 15:37:07 +0530

    Update BorrowBookSearch.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/BorrowBookSearch.jsx
+++ client/src/comportments/Books/BorrowBookSearch.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import BorrowRequests from "./BorrowRequests"
 
 const BorrowBookSearch = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    const [buttonValue, SetButtonValue] = useState(0)
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
+    // check form is submit or not
+    const [IsFormSubmited, SetIsFormSubmited] = useState(false)
+    
+
   return (
     <div>BorrowBookSearch</div>
   )
