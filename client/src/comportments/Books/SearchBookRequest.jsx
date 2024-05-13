f395547677fc3185354996ab2fb946ad54fd0a38
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-13 11:17:47 +0530

    Update SearchBookRequest.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Books/SearchBookRequest.jsx
+++ client/src/comportments/Books/SearchBookRequest.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const SearchBookRequest = () => {
-  return (
-    <div>SearchBookRequest</div>
-  )
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    if(RoleUser !== "SuperAdmin") {
+        return (
+            <div>SearchBookRequest</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
+
 }
 
 export default SearchBookRequest
