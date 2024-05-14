ba65758a96c8e013829f884f662be5b34f2ae660
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-14 10:32:41 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -190,7 +190,7 @@ const HomePage = () => {
         <div className={`lg:px-24 px-10 py-20 ${Darkmode === true ? 'dark-mode duration-500' : 'light-mode bg-gray-200 duration-500'}`}>
             <h1 className={`text-center text-4xl font-semibold text-gray-500 mb-16 ${Darkmode === false ? 'text-gray-500' : 'text-white'}`}>Our Services</h1>
             <div className="lg:flex justify-between my-4">
-                <div className={`bg-white rounded py-10 px-8 shadow-md mx-2 lg:my-0 my-2 w-full  ${Darkmode === false ? 'text-gray-500' : 'text-gray-200 bg-[]'}`}>
+                <div className={`bg-white rounded py-10 px-8 shadow-md mx-2 lg:my-0 my-2 w-full  ${Darkmode === false ? 'text-gray-500' : 'text-gray-200 bg-[#2c3646]'}`}>
                     <div className="flex">
                         <span className='my-4 mr-4'><MyIcons name='book' size='large'></MyIcons></span>
                         <div className="">
@@ -199,7 +199,7 @@ const HomePage = () => {
                         </div>
                     </div>
                 </div>
-                <div className={`bg-white rounded py-10 px-8 shadow-md mx-2 lg:my-0 my-2 w-full  ${Darkmode === false ? 'text-gray-500' : 'text-white'}`}>
+                <div className={`bg-white rounded py-10 px-8 shadow-md mx-2 lg:my-0 my-2 w-full  ${Darkmode === false ? 'text-gray-500' : 'text-gray-200 bg-[#2c3646]'}`}>
                     <div className="flex">
                         <span className='my-4 mr-2'><MyIcons name='globe' size='large'></MyIcons></span>
                         <div className="">
@@ -210,10 +210,10 @@ const HomePage = () => {
                 </div>
             </div>
         </div>
-        <div className="bg-white lg:mx-20 mx-6 my-24">
-            <h1 className="text-center text-4xl font-semibold text-gray-500 my-8">Latest Books</h1>
+        <div className={`lg:px-20 px-6 py-24  ${Darkmode === true ? 'dark-mode duration-500' : 'bg-white light-mode duration-500'}`}>
+            <h1 className={`text-center text-4xl font-semibold text-gray-500 my-8 ${Darkmode === false ? 'text-gray-500' : 'text-white'}`}>Latest Books</h1>
             <div className="lg:flex my-24">
-                <div className="bg-gray-200 py-8 px-4 mx-4 lg:my-0 my-12 rounded-lg shadow-xl lg:w-full">
+                <div className={`bg-gray-200 py-8 px-4 mx-4 lg:my-0 my-12 rounded-lg shadow-xl lg:w-full `}>
                     <div className="my-8 mx-8">
                         <div className="">
                             <p className='font-semibold'>Book Name: </p>
