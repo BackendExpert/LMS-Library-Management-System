587b3d9df53b5ab51c4ab4a178ec31a1b823ae0d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-15 09:49:06 +0530

    Update MyBorroedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyBorroedBooks.jsx
+++ client/src/comportments/MyProfile/MyBorroedBooks.jsx
@@ -11,9 +11,46 @@ const MyBorroedBooks = () => {
   const RoleUser = secureLocalStorage.getItem("Login1");
   const EmailUser = secureLocalStorage.getItem("login2");
 
-  return (
-    <div>MyBorroedBooks</div>
-  )
+  if(RoleUser !== null && EmailUser !== null) {
+    return (
+        <div>
+            <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+                <h1 className="font-semibold text-gray-500 text-xl">My Borrowed Books</h1>
+
+                    <div class="relative overflow-x-auto my-8">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" class="px-6 py-3">
+                                                Book ISBN Number
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Borrower Email
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Borrow at
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Status
+                                            </th>
+                                        </tr>
+        
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>  
+            </div>
+        </div>
+    )
+  }
+  else{
+      useEffect(() => {
+          localStorage.clear()
+          navigate('/')
+      }, [])
+  }
 }
 
 
