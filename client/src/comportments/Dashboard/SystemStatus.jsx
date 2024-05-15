249c7d05cbd2441d26fb0aebc859b5f2a2760eb2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-15 13:58:30 +0530

    Update SystemStatus.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/SystemStatus.jsx
+++ client/src/comportments/Dashboard/SystemStatus.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SystemStatus = () => {
   return (
-    <div>SystemStatus</div>
+    <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+        <h1 className="font-semibold text-gray-500 text-xl">System Status</h1>
+    </div>
   )
 }
 
