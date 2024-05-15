33fa1470785465fe797243fc48ad5d86e9feb4bd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-15 09:39:26 +0530

    Update MyBorroedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyBorroedBooks.jsx
+++ client/src/comportments/MyProfile/MyBorroedBooks.jsx
@@ -6,7 +6,7 @@ const MyBorroedBooks = () => {
   )
 }
 
-// download data 
+
 // only borrowed status + return date + conframretrun data + still dates to retun book
 // check date to retrun date 7,17,21 and last data tommrow -> sending emails
 
