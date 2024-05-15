13515b3f7b4c2f12ca018f6b55d01094eb0ea06d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-15 07:21:54 +0530

    Update ReqsProcessingMy.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/ReqsProcessingMy.jsx
+++ client/src/comportments/MyProfile/ReqsProcessingMy.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+
 
 const ReqsProcessingMy = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    
   return (
     <div>
         <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
