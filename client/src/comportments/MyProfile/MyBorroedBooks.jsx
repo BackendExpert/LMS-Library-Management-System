8ab37d9bcf2b0ab94a9f0710f568058038d37615
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-15 09:48:03 +0530

    Update MyBorroedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyBorroedBooks.jsx
+++ client/src/comportments/MyProfile/MyBorroedBooks.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+
 
 const MyBorroedBooks = () => {
+  const navigate = useNavigate() 
+  //curent login user
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
+
   return (
     <div>MyBorroedBooks</div>
   )
