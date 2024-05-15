fbc3ccf2d32349e19bd1abd641eba940158da09a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-15 07:21:27 +0530

    Update ReqsProcessingMy.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/ReqsProcessingMy.jsx
+++ client/src/comportments/MyProfile/ReqsProcessingMy.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ReqsProcessingMy = () => {
   return (
-    <div>ReqsProcessingMy</div>
+    <div>
+        <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+                <h1 className="font-semibold text-gray-500 text-xl">Add New Book</h1>
+        </div>
+    </div>
   )
 }
 
