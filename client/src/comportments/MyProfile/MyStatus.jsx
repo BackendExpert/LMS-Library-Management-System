e36da1e6f1553b1d9baa53a023161a20fa85ada5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 09:36:41 +0530

    Update MyStatus.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyStatus.jsx
+++ client/src/comportments/MyProfile/MyStatus.jsx
@@ -4,6 +4,15 @@ const MyStatus = () => {
   return (
     <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
         <h1 className="font-semibold text-gray-500 text-xl">Download My Status</h1>
+
+        <div className="lg:grid grid-cols-3 gap-4 my-8">
+            <div className="">
+              <h1 className="text-gray-500">My All Borrowed Books</h1>
+              <div className="m-2">
+                <button className='py-2 px-8 bg-blue-500 rounded text-white duration-500 hover:bg-blue-600'>Download Data</button>
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
