771e99e09074a44dec63fe36f3be64f91bf1d867
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 06:44:16 +0530

    Update MyAllBorrowBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyAllBorrowBooks.jsx
+++ client/src/comportments/MyProfile/MyAllBorrowBooks.jsx
@@ -14,7 +14,36 @@ const MyAllBorrowBooks = () => {
     return (
       <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
           <h1 className="font-semibold text-gray-500 text-xl">My All Books</h1>
-  
+              <div class="relative overflow-x-auto my-8">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Book ISBN Number
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Borrow at
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Status
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Return at
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Had ro return at
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        More Infor
+                                    </th>
+                                </tr>
+
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>  
       </div>
     )
   }
