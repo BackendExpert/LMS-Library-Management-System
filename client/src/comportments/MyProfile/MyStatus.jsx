f20ab5a3e75bc1e233c29ec1b6969dcc56c13c49
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 10:04:53 +0530

    Update MyStatus.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyStatus.jsx
+++ client/src/comportments/MyProfile/MyStatus.jsx
@@ -20,7 +20,7 @@ const MyStatus = () => {
       const url = window.URL.createObjectURL(new Blob([res.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', 'data.csv');
+      link.setAttribute('download', 'MyAllBooks.csv');
       document.body.appendChild(link);
       link.click();
       link.remove();
