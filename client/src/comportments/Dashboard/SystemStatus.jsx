7d9babc50991ebea644ca99cab6e3a7dd6a6f0d3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 10:18:27 +0530

    Update SystemStatus.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/SystemStatus.jsx
+++ client/src/comportments/Dashboard/SystemStatus.jsx
@@ -14,6 +14,17 @@ const SystemStatus = () => {
     return (
       <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
           <h1 className="font-semibold text-gray-500 text-xl">System Status</h1>
+
+          <div className="my-8">
+            <div className="lg:grid grid-cols-3 gap-4">
+              <div className="my-2">
+                <h1 className="text-gray-500">Books</h1>
+                <div className="m-2">
+                  <button onClick={() => headleDownloadBooks()} className='py-2 px-8 bg-blue-500 rounded text-white duration-500 hover:bg-blue-600'>Download Data</button>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     )
   }
