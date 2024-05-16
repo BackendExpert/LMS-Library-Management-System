7c339cda79b485110c28beab93c4156d12a0add0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 09:42:14 +0530

    Update MyStatus.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyStatus.jsx
+++ client/src/comportments/MyProfile/MyStatus.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup';
+import MyAllBorrowBooks from "./MyAllBorrowBooks";
+import axios from "axios";
+
 
 const MyStatus = () => {
-  return (
-    <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
-        <h1 className="font-semibold text-gray-500 text-xl">Download My Status</h1>
+  const navigate = useNavigate() 
+  //curent login user
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
 
-        <div className="lg:grid grid-cols-3 gap-4 my-8">
-            <div className="">
-              <h1 className="text-gray-500">My All Borrowed Books</h1>
-              <div className="m-2">
-                <button className='py-2 px-8 bg-blue-500 rounded text-white duration-500 hover:bg-blue-600'>Download Data</button>
+  // download my data borrowed all book
+  const headleDownloadData = (id) => {
+    axios.post('http://localhost:8081/DownloadMyAllBooks/' + id, { responseType: 'blob' })
+    
+  }
+
+  
+
+  if(RoleUser !== null && EmailUser !== null){
+    return (
+      <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+          <h1 className="font-semibold text-gray-500 text-xl">Download My Status</h1>
+  
+          <div className="lg:grid grid-cols-3 gap-4 my-8">
+              <div className="">
+                <h1 className="text-gray-500">My All Borrowed Books</h1>
+                <div className="m-2">
+                  <button onClick={() => headleDownloadData(EmailUser)} className='py-2 px-8 bg-blue-500 rounded text-white duration-500 hover:bg-blue-600'>Download Data</button>
+                </div>
               </div>
-            </div>
-        </div>
-    </div>
-  )
+          </div>
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+        localStorage.clear()
+        navigate('/')
+    }, [])
+  }
 }
 
 export default MyStatus
