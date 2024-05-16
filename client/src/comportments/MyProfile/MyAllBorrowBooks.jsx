65dfb5fa2a1a74b22a93a31bbed4818e9b25b851
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 06:42:24 +0530

    Update MyAllBorrowBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyProfile/MyAllBorrowBooks.jsx
+++ client/src/comportments/MyProfile/MyAllBorrowBooks.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const MyAllBorrowBooks = () => {
-  return (
-    <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
-        <h1 className="font-semibold text-gray-500 text-xl">My All Books</h1>
-    </div>
-  )
+  const navigate = useNavigate() 
+  //curent login user
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
+
+  if(RoleUser !== null && EmailUser !== null){
+    return (
+      <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
+          <h1 className="font-semibold text-gray-500 text-xl">My All Books</h1>
+  
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+        localStorage.clear()
+        navigate('/')
+    }, [])
+  }
+
 }
 
 export default MyAllBorrowBooks
