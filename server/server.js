3ba75c13a64b01b8f4714c724d8d0316003a174d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 09:53:04 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1828,6 +1828,15 @@ app.get('/CountAllBookMy/:id', (req, res) => {
 app.get('/DownloadMyAllBooks/:id', (req, res) => {
     const userEmail = req.params.id
     
+    const sql = "SELECT * book_borrow_request WHERE borrowEmail = ?"
+
+    connection.query(sql, [userEmail], (err, results, fields) => {
+        if (err) throw err
+
+        const csv = parse(results);
+        fs.writeFileSync('data.csv', csv);
+        res.download('data.csv', 'data.csv');
+    })
 })
 
 // function onServerStart() {
